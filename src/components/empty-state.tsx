df7fe7b14ea368656f4223c2d33abf9fe1fbbffe
Pulkit Gangwar <pulkitgangwar0@gmail.com>
2025-12-13 14:33:26 +0530

    added meetings table

--- src/components/empty-state.tsx
+++ src/components/empty-state.tsx
@@ -8,8 +8,14 @@ interface Props {
 export const EmptyState = ({ title, description }: Props) => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <Image src="./logo.svg" alt="Empty" width={180} height={180} />
-      <div className="flex flex-col gap-y-6 max-w-md mx-auto text-center m-3">
+      <Image
+        src="./empty-state.svg"
+        alt="Empty"
+        width={180}
+        height={180}
+        className="opacity-40"
+      />
+      <div className="flex flex-col gap-y-6 max-w-md mx-auto text-center m-5">
         <h6 className="text-lg font-medium">{title}</h6>
         <p className="text-sm text-muted-foreground">{description}</p>
       </div>
