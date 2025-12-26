815d85ffa8c728fc23d77c010168c5bf7178e83a
Pulkit Gangwar <pulkitgangwar0@gmail.com>
2025-12-26 21:17:04 +0530

    added transcript and agent chat

---
+++ src/modules/meetings/ui/components/chat-ui.tsx
@@ -0,0 +1,80 @@
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { useMemo } from "react";
+import type { Channel as StreamChannel } from "stream-chat";
+
+import {
+  Channel,
+  Chat,
+  MessageInput,
+  MessageList,
+  Thread,
+  useCreateChatClient,
+  Window,
+} from "stream-chat-react";
+
+import "stream-chat-react/dist/css/v2/index.css";
+
+interface ChatUIProps {
+  meetingId: string;
+  meetingName: string;
+  userId: string;
+  userName: string;
+  userImage: string | undefined;
+}
+
+export const ChatUI = ({
+  meetingId,
+  userId,
+  userName,
+  userImage,
+}: ChatUIProps) => {
+  const trpc = useTRPC();
+  const { mutateAsync: generateChatToken } = useMutation(
+    trpc.meetings.generateChatToken.mutationOptions()
+  );
+
+  const client = useCreateChatClient({
+    apiKey: process.env.NEXT_PUBLIC_STREAM_CHAT_API_KEY!,
+    tokenOrProvider: generateChatToken,
+    userData: {
+      id: userId,
+      name: userName,
+      image: userImage,
+    },
+  });
+
+  const channel: StreamChannel | undefined = useMemo(() => {
+    if (!client) return undefined;
+
+    return client.channel("messaging", meetingId, {
+      members: [userId],
+    });
+  }, [client, meetingId, userId]);
+
+  if (!client || !channel) {
+    return (
+      <LoadingState
+        title="Loading Chat"
+        description="This may take a few seconds"
+      />
+    );
+  }
+
+  return (
+    <div className="bg-white rounded-lg border overflow-hidden">
+      <Chat client={client}>
+        <Channel channel={channel}>
+          <Window>
+            <div className="flex-1 overflow-y-auto max-h-[calc(100vh-23rem)] border-b">
+              <MessageList />
+            </div>
+            <MessageInput />
+          </Window>
+          <Thread />
+        </Channel>
+      </Chat>
+    </div>
+  );
+};
